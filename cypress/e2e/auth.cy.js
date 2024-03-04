fd47d05c67676073ee1dc8adce2f47ea4d304706
GermanPaul <german.kalmykov@mail.ru>
2024-03-04 13:26:05 +0300

    final2

--- cypress/e2e/auth.cy.js
+++ cypress/e2e/auth.cy.js
@@ -1,14 +1,11 @@
 describe("auth checking", () => {
   it("login and logout", () => {
-    // Регистрация пользователя
     cy.visit("http://localhost:3000/");
     cy.get("a").contains("Зарегистрироваться").click();
     cy.location("pathname").should("include", "registration");
     cy.get('input[placeholder="Введите email"]').type("test@gmail.com");
     cy.get('input[placeholder="Введите пароль"]').type("Query12345");
     cy.get("button").contains("Зарегистрироваться").click();
-
-    // Авторизация пользователя
     cy.get('input[placeholder="Введите email"]').type("{selectall}{backspace}");
     cy.get('input[placeholder="Введите email"]').type("test@gmail.com");
     cy.get('input[placeholder="Введите пароль"]').type("{selectall}{backspace}");
